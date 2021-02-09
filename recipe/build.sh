218e88894a5b40b4af8ee22854beb0b8ab8a46d2
Bastian Zimmermann <10774221+BastianZim@users.noreply.github.com>
2021-02-09 05:50:35 +0100

    Support macOS and update to 0.1.95 (#19)
    * Update to 0.1.95
    
    * Remove space
    
    * Reset build number
    
    * Fix specifier
    
    * Remove gperftools
    
    * Add upstream tests
    
    * Add osx build script
    
    * Remove sudo on mac
    
    Due to https://github.com/conda-forge/conda-forge.github.io/issues/810
    
    * MNT: Re-rendered with conda-build 3.21.4, conda-smithy 3.8.6, and conda-forge-pinning 2021.02.07.11.21.33
    
    Co-authored-by: conda-forge-linter <github-actions@email.com>

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,22 +1,33 @@
 mkdir build
 cd build
 
-export LD_LIBRARY_PATH=${PREFIX}/lib
-export CPATH=${PREFIX}/include
-export INCLUDE=${PREFIX}/include
-export LIBRARY_PATH=${PREFIX}/lib
+if [[ "${target_platform}" == linux-* ]]; then
 
-cmake \
-    -DCMAKE_INSTALL_PREFIX=$PREFIX \
-    -DCMAKE_INSTALL_LIBDIR=$PREFIX/lib \
-    -DCMAKE_AR="${AR}" \
-    -DSPM_ENABLE_TCMALLOC=OFF \
-    -S ..
+    export LD_LIBRARY_PATH=${PREFIX}/lib
+    export CPATH=${PREFIX}/include
+    export INCLUDE=${PREFIX}/include
+    export LIBRARY_PATH=${PREFIX}/lib
+
+    cmake \
+        -DCMAKE_INSTALL_PREFIX=$PREFIX \
+        -DCMAKE_INSTALL_LIBDIR=$PREFIX/lib \
+        -DCMAKE_AR="${AR}" \
+        -DSPM_ENABLE_TCMALLOC=OFF \
+        -S ..
+
+elif [[ "${target_platform}" == osx-* ]]; then
+
+    cmake .. -DSPM_ENABLE_SHARED=OFF -DCMAKE_INSTALL_PREFIX=../..
+    
+fi
 
 make -j ${CPU_COUNT}
 make install
 
-ldconfig -v -N
+if [[ "${target_platform}" == linux-* ]]; then
+    ldconfig -v -N
+fi
+
 cd $SRC_DIR/python
 ${PYTHON} setup.py build
 ${PYTHON} setup.py install
